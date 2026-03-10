a64ec858d81bbb8861f7fd97c59fbbdfdd6f441b
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-03-09 21:04:38 -0300

    calculo de imc em andamento

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.calculadoraimc
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,17 +16,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -76,6 +87,14 @@ fun IMCScreen(modifier: Modifier = Modifier) {
             )
         }
 
+        var altura by remember {
+            mutableStateOf("")
+        }
+
+        var peso by remember {
+            mutableStateOf("")
+        }
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -90,14 +109,92 @@ fun IMCScreen(modifier: Modifier = Modifier) {
                 colors = CardDefaults.cardColors(
                     containerColor = Color(0xFFF9F6F6)
                 ),
-                elevation = CardDefaults.cardElevation(4.dp)        ,
-            ) { }
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Seus Dados",
+                        color = colorResource(R.color.cor_app),
+                        fontSize = 28.sp
+                    )
+
+                    TextField(
+                        value = altura,
+                        onValueChange = {
+                            altura = it
+                        },
+
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        placeholder = {
+                            Text(
+                                text = "Altura"
+                            )
+                        },
+                        singleLine = true
+                    )
+
+                    TextField(
+                        value = peso,
+                        onValueChange = {
+                            peso = it
+                        },
+
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        placeholder = {
+                            Text(
+                                text = "Peso"
+                            )
+                        },
+                        singleLine = true
+                    )
+
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .background(colorResource(R.color.cor_app))
+                    ) {
+                        Text(
+                            text = "Calcular",
+                            color = Color.White
+                        )
+                    }
+                }
+            }
         }
 
+        val imc = 0
+
         Row(
-
+            modifier = modifier
+                .fillMaxWidth()
         ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "$imc"
+                )
 
+                if (imc < 18.5){
+                    Text(text = "Abaixo do peso.")
+                }else if (imc < 25){
+                    Text(text = "Peso ideal.")
+                }else if (imc < 30){
+                    Text(text = "Levemente acima do peso.")
+                }else if (imc < 35){
+                    Text(text = "Obesidade grau 1.")
+                }else if (imc < 40){
+                    Text(text = "Obesidade grau 2.")
+                }else{
+                    Text(text = "Obesidade grau 3.")
+                }
+            }
         }
     }
 }
